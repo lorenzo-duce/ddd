2e485c8388685ff5f91ed1daa7d02bb178b853a6
lorenzo-duce <masi_lorenzo@icloud.com>
2026-09-01 18:57:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,92 @@
 # Import
 from flask import Flask, render_template,request, redirect
-# Importare la libreria del database
+# Collegare la libreria del database
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# Collegamento con SQLite
+# Impostare la chiave segreta per la sessione
+app.secret_key = 'il_mio_super_segreto_1234'
+# Connettere SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Creare un DB
-db = SQLAlchemy(app )
+# Creare il DB
+db = SQLAlchemy(app)
+# Creare la tabella
 
-#Consegna #1. Creare una tabella del DB
+class Card(db.Model):
+    # Creazione delle colonne
+    # id
+    id = db.Column(db.Integer, primary_key=True)
+    # Titolo
+    title = db.Column(db.String(100), nullable=False)
+    # Sottotitolo
+    subtitle = db.Column(db.String(300), nullable=False)
+    # Testo
+    text = db.Column(db.Text, nullable=False)
+    # La mail del proprietario della scheda
+    user_email = db.Column(db.String(100), nullable=False)
 
-
-
-
-
-
-
-
-
-
-
-# Esecuzione della pagina con il contenuto
-@app.route('/')
-def index():
-    # Visualizzazione degli oggetti del DB
-    # Consegna #2. Visualizzare gli oggetti del DB in index.html
+    # Visualizzazione dell'oggetto e dell'id
+    def __repr__(self):
+        return f'<Card {self.id}>'
     
 
-    return render_template('index.html',
-                           #cards = cards
+#Consegna #1. Creare la tabella User
 
-                           )
+
+# Esecuzione della pagina dei contenuti
+@app.route('/', methods=['GET','POST'])
+def login():
+        error = ''
+        if request.method == 'POST':
+            form_login = request.form['email']
+            form_password = request.form['password']
+            
+            #Consegna #4. Implementare l'autorizzazione
+            
+         
+        else:
+            return render_template('login.html')
+
+
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Consegna #3. Implementare la registrazione dell'utente.
+        
+
+        
+        return redirect('/')
+    
+    else:    
+        return render_template('registration.html')
+
+
+# Esecuzione della pagina dei contenuti
+@app.route('/index')
+def index():
+    # Consegna #4. Assicurarsi gli utenti vedano solo le proprie schede
+    cards = Card.query.order_by(Card.id).all()
+    return render_template('index.html', cards=cards)
 
 # Esecuzione della pagina con la scheda
 @app.route('/card/<int:id>')
 def card(id):
-    # Consegna #2. Mostrare la scheda giusta in base al suo id
-    
+    card = Card.query.get(id)
 
     return render_template('card.html', card=card)
 
-# Esecuzione della pagina e creazione della scheda
+# Esecuzione della pagina di creazione della voce
 @app.route('/create')
 def create():
     return render_template('create_card.html')
 
-# Il modulo della scheda
+# Il modulo di creazione della scheda
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
     if request.method == 'POST':
@@ -56,16 +94,14 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        # Consegna #2. Creare un modo per memorizzare i dati nel DB
-        
+        # Consegna #4. Fare in modo che la creazione avvenga per contro dell'utente corretto
+        card = Card(title=title, subtitle=subtitle, text=text)
 
-
-
-
-        return redirect('/')
+        db.session.add(card)
+        db.session.commit()
+        return redirect('/index')
     else:
         return render_template('create_card.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
